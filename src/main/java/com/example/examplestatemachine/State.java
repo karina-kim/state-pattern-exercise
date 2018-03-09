088fff6e396cdc60b3e4627209241f3e6b5d239d
karina-kim <37209625+karina-kim@users.noreply.github.com>
2018-03-09 16:04:19 +0600

    Add files via upload

--- src/main/java/com/example/examplestatemachine/State.java
+++ src/main/java/com/example/examplestatemachine/State.java
@@ -2,11 +2,32 @@ package com.example.examplestatemachine;
 
 public abstract class State {
     protected StateContext sc;
+<<<<<<< HEAD
+    protected boolean accept = true;
+    String actions;
+    
+    public void actionA() {
+    	actions=actions+"A";
+    	if(actions.startsWith("A")) {
+    		accept=true;
+    	}
+    	else accept=false;
+    }
+    
+    public void actionB() {
+    	actions=actions+"B";
+    	if(actions.startsWith("B")) {
+    		accept=true;
+    	}
+    	else accept=false;
+    }
+=======
     protected boolean accept = false;
     
     public void actionA() {}
     
     public void actionB() {}
+>>>>>>> b1bc2bf765776c80e4525de811e9f4b4ede0ea0d
     
     public boolean isAccept() {
         return this.accept;
---
+++ src/main/java/com/example/examplestatemachine/StateContext.java
@@ -0,0 +1,10 @@
+package com.example.examplestatemachine;
+
+public class StateContext extends State{
+	
+	StateContext(){
+		actions="";
+		accept=false;
+	}
+	
+}
